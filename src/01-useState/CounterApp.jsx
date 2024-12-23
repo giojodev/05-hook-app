41791eb58581f8a332643663b8cdb1b3e45e1052
giojodev <whoisyio@gmail.com>
2024-12-23 17:40:22 -0600

    Correccion de ustestate

--- src/01-useState/CounterApp.jsx
+++ src/01-useState/CounterApp.jsx
@@ -1,12 +1,30 @@
 import { useState } from "react"
 
 export const CounterApp = () => {
-    const [counter, setCounter] = useState(10)
+    const [state, setCounter] = useState({
+        counter1:10,
+        counter2:20,
+        counter3:30,
+    })
+
+    const {counter1,counter2,counter3} = state;
     return (
         <>
-            <h1>Counter: {counter}</h1>
+            <h1>Counter: {counter1}</h1>
+            <h2>Counter: {counter2}</h2>
+            <h3>Counter: {counter3}</h3>
             <hr />
-            <button className="btn btn-primary" onClick={() => setCounter(counter + 1)}>+1</button>
+            <button className="btn btn-primary" onClick={() => 
+                {
+                setCounter({
+                    ...state,
+                    counter2:counter2 + 1,
+                    counter3:counter3 + 1
+                
+                })
+               
+                }
+            }>+1</button>
         </>
     )
 }
