f58b5bc8755397e9c41eca2ed64d5727771def0c
giojodev <whoisyio@gmail.com>
2024-12-24 18:10:18 -0600

    Agregar funciones al customhook

--- src/01-useState/CoutnerWithCustomHook.jsx
+++ src/01-useState/CoutnerWithCustomHook.jsx
@@ -1,16 +1,17 @@
 import {useCounter} from '../hooks/useCounter.js';
 
 export const CounterWithCustomHook = () => {
-    const {counter} = useCounter(100);
+    const {counter,increment,decrement,resetValue} = useCounter(100);
+
     return(
         <>
         
             <h1>
                 Counter With CustomHook: {counter}
             </h1>
-            <button className="btn btn-primary">+1</button>
-            <button className="btn btn-primary">Reset</button>
-            <button className="btn btn-primary">-1</button>
+            <button className="btn btn-primary" onClick={()=> increment(2) }>+1</button>
+            <button className="btn btn-primary" onClick={resetValue} >Reset</button>
+            <button className="btn btn-primary" onClick={()=> decrement(2)}>-1</button>
         </>
     )
 }
