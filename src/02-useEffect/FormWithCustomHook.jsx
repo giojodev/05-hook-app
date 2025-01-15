0bf971aa2acf3777fb9fe5296fce62371ee8aeb3
Giovanni <whoisyio@gmail.com>
2025-01-14 21:43:49 -0600

    Cambio en el useEffect

---
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,69 @@
+import { useEffect,useState } from "react";
+import { Message } from "./Message";
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        username:'strider',
+        email:'whoisyio@gmail.com'
+    });
+    const {username, email} = formState;
+
+    const onInputChange = ({target}) => {
+        const {name, value} = target;
+        setFormState({
+            ...formState,
+            [name]: value
+        });
+    }
+    //Se ejecutara 1 vez el useEffect al montar el componente
+    useEffect(() => {
+        // console.log('Hey');
+    }, []);
+    //Se ejecutar cada vez que el formsState cambie
+    useEffect(() => {
+        // console.log('formState');
+    }, [formState]);
+    //Se ejecutara cada vez que el email cambie
+    useEffect(() => {
+        // console.log('email');
+    }, [email]);
+    
+    // useEffect(() => {
+    //   first
+    
+    //   return () => {
+    //     second
+    //   }
+    // }, [third])
+    
+
+
+    return (
+        <>
+            <h1>useEffect</h1>
+            <hr></hr>
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={onInputChange}
+            />
+            <input
+                type="email"
+                className="form-control mt-2"
+                placeholder="example@google.com"
+                name="email"
+                value={email}
+                onChange={onInputChange}
+            />
+
+            {
+                (username === 'strider2') && <Message />
+            }
+            
+        </>
+    )
+}
--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -2,18 +2,23 @@
 import { useEffect } from "react"
 export const Message = () => {
     useEffect(() => {
+      const [coords, setCoords] = useState({x:0, y:0})
         //Este mensaje aparecera cuando el componente se monte o aparezca en la pagina
-      console.log('Message Mounted')
-    
+      const onMouseMove = ({x,y}) => {  
+        
+        setCoords({x,y})
+      }
+      window.addEventListener('mousemove', onMouseMove);
       return () => {
         //Este mensaje aparecera cuando el componente se destruya o desaparezca de la pagina
-        console.log('Message UnMounted')
+        window.removeEventListener('mousemove', onMouseMove);
       }
     }, [])
     
   return (
     <>
       <h3>Usuario ya existe</h3>
+      <div>{JSON.stringify(coords)}</div>
     </>
   )
 }
