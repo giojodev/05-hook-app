2a166963d2b7ff80e5b9a9c97e97408688af67bb
Giovanni <whoisyio@gmail.com>
2025-03-09 21:24:05 -0600

    Fix indentation in Small component

--- src/06-memos/Small.jsx
+++ src/06-memos/Small.jsx
@@ -1,10 +1,10 @@
-import { memo } from "react"
+    import { memo } from "react"
 
-export const Small = memo(({ value }) => {
-    console.log('Me volví a llamar :(')
-    return (
-        <small>{value}</small>
+    export const Small = memo(({ value }) => {
+        console.log('Me volví a llamar :(')
+        return (
+            <small>{value}</small>
+        )
+    }
     )
-}
-)
 
