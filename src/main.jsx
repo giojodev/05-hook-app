028001f7d5bf2d4cdf717616934a7b715b3c06dc
Giovanni <whoisyio@gmail.com>
2025-03-09 22:02:06 -0600

    Replace Memorize component with MemoHook for optimized rendering

--- src/main.jsx
+++ src/main.jsx
@@ -10,9 +10,10 @@ import './index.css'
 // import MultipleCustomHooks from './03-examples/MultipleCustomHooks'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { Layout } from './05-useLayoutEffect/Layout'
-import Memorize from './06-memos/Memorize'
+// import Memorize from './06-memos/Memorize'
+ import {MemoHook} from './06-memos/MemorizeHook'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Memorize />
+    <MemoHook />
   </StrictMode>,
 )
