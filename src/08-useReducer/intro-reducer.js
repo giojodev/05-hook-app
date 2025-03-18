f8c8cbc6dbcf35e817eef60b0126285e92a3b7f9
Giovanni <whoisyio@gmail.com>
2025-03-17 21:53:27 -0600

    Cambio en funcion de reducer

--- src/08-useReducer/intro-reducer.js
+++ src/08-useReducer/intro-reducer.js
@@ -1,1 +1,32 @@
-console.log('hola mundo');
+
+const initialState = [{
+    id: 1,
+    todo:'Comprar pan',
+    done: false
+}
+];
+
+const todoReducer = (state = initialState,action = {}) => {
+
+
+    if(action?.type === 'add todo'){
+        return [...state,action.payload];
+    }
+
+    return state;
+}
+
+let todos = todoReducer();
+
+const newTodo = {
+    id: 2,
+    todo:'Comprar leche',
+    done: false
+}
+
+const addTodoAtion = {
+    type: 'add todo',
+    payload: newTodo
+}
+todos = todoReducer(todos,addTodoAtion);
+console.log(todos);
