fdad5c550da185f447debb99a9f03e20c66c7c5b
Giovanni <whoisyio@gmail.com>
2025-03-30 11:06:48 -0600

    Refactor TodoApp to improve UI and add functionality for adding and deleting tasks

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -12,17 +12,41 @@ const  initialState = [{
     done: false
 }]
 export const TodoApp = () => {
-    const [state, dispatch] = useReducer(todoReducer, initialState)
+    const [todos, dispatch] = useReducer(todoReducer, initialState)
   
     return (
     <>
-      <h1>TodoApp</h1>
+      <h1>TodoApp 10,  <small>pendientes: 2 </small></h1>
       <hr />
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-      </ul>
+      <div className="row">
+        <div className="col-7">
+            <ul className="list-group">
+              {
+                todos.map(todo => (
+                  <li className="list-group-item d-flex justify-content-between">
+                    <span className="align -self-center">Item 1</span>
+                    <button className="btn btn-danger">Borrar</button>
+                  </li>
+                 
+                ))
+              }
+            </ul>
+        </div>
+
+        <div className="col-5">
+          <h4>Agregar TODO</h4>
+          <hr />
+          <form action="./">
+            <input type="text"
+                  placeholder="Agregar tarea" 
+                  className="form-control"/>
+                  <button type="submit"
+                  className="btn btn-outline-primary mt-1 btn-block">
+                    Agregar
+                  </button>
+          </form>
+        </div>
+      </div>
     </>
   )
 }
