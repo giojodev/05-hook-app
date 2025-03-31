f045a26ec661e6b0b14bddca8039bdc817266814
Giovanni <whoisyio@gmail.com>
2025-03-30 22:31:44 -0600

    Add TodoAdd and TodoList components; refactor TodoApp for improved task management

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,6 +1,7 @@
 import { useReducer } from "react"
 import { todoReducer } from "./todoReducer"
-
+import { TodoList } from "./TodoList"
+import { TodoAdd } from "./TodoAdd"
 const  initialState = [{
     id: new Date().getTime(),
     desc: 'Aprender React',
@@ -13,38 +14,22 @@ const  initialState = [{
 }]
 export const TodoApp = () => {
     const [todos, dispatch] = useReducer(todoReducer, initialState)
-  
+    const handleNewTodo = (todo) => {
+        console.log({todo})
+    }
     return (
     <>
       <h1>TodoApp 10,  <small>pendientes: 2 </small></h1>
       <hr />
       <div className="row">
         <div className="col-7">
-            <ul className="list-group">
-              {
-                todos.map(todo => (
-                  <li className="list-group-item d-flex justify-content-between">
-                    <span className="align -self-center">Item 1</span>
-                    <button className="btn btn-danger">Borrar</button>
-                  </li>
-                 
-                ))
-              }
-            </ul>
+            <TodoList todos={todos} />
         </div>
 
         <div className="col-5">
           <h4>Agregar TODO</h4>
           <hr />
-          <form action="./">
-            <input type="text"
-                  placeholder="Agregar tarea" 
-                  className="form-control"/>
-                  <button type="submit"
-                  className="btn btn-outline-primary mt-1 btn-block">
-                    Agregar
-                  </button>
-          </form>
+            <TodoAdd onNewTodo={handleNewTodo}/>
         </div>
       </div>
     </>
