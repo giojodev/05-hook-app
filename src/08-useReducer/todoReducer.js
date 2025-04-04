5708fad0475a4e39238b74328061ecc9c6049c7c
Giovanni <whoisyio@gmail.com>
2025-04-03 22:06:53 -0600

    Refactor TodoApp to implement action type constants for adding todos

--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -1,6 +1,6 @@
 export const todoReducer = (initialState = [], action) => {
     switch (action.type) {
-        case 'add':
+        case '[TODO] Add Todo':
             return [...initialState, action.payload]
         default:
             return initialState
