db456a625cacf574da16ebcf0cc21e0ab0ddcd3d
Giovanni <whoisyio@gmail.com>
2025-04-07 22:41:54 -0600

    Implement localStorage persistence and add delete functionality in TodoApp

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,20 +1,29 @@
-import { useReducer } from "react"
+import { useEffect, useReducer } from "react"
 import { todoReducer } from "./todoReducer"
 import { TodoList } from "./TodoList"
 import { TodoAdd } from "./TodoAdd"
-const  initialState = [{
-    id: new Date().getTime(),
-    desc: 'Aprender React',
-    done: false
-},
-{
-    id: new Date().getTime() + 1000,
-    desc: 'Aprender Mongo',
-    done: false
-}]
+
+const  initialState = [
+//   {
+//     id: new Date().getTime(),
+//     desc: 'Aprender React',
+//     done: false
+// }
+]
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || []
+} 
+
+
 export const TodoApp = () => {
-    const [todos, dispatch] = useReducer(todoReducer, initialState)
+    const [todos, dispatch] = useReducer(todoReducer, initialState,init)
     
+    useEffect(() => {
+      localStorage.setItem('todos', JSON.stringify(todos))
+    
+  }, [todos])
+
     const handleNewTodo = (todo) => {
         const action = {
             type: '[TODO] Add Todo',
@@ -22,13 +31,21 @@ export const TodoApp = () => {
         }
         dispatch(action)
     }
+
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        })
+    }
+
     return (
     <>
       <h1>TodoApp 10,  <small>pendientes: 2 </small></h1>
       <hr />
       <div className="row">
         <div className="col-7">
-            <TodoList todos={todos} />
+            <TodoList todos={todos} onDeleteTodo = {handleDeleteTodo} />
         </div>
 
         <div className="col-5">
